a23b5720c8e58f47c86e8137ed0450415ac28ca6
João Vitor <joaovitor.jsr@gmail.com>
2021-08-27 19:27:33 -0300

    new

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -15,21 +15,21 @@ export class CourseListComponent implements OnInit{
           {
             id: 1,
             name: 'Angular: Formes',
-            imageUrl: '',
+            imageUrl: '/assets/images/forms.png',
             price: 99.99,
             code: 'XPS-8796',
             duration: 120,
-            rating: 5.4, 
+            rating: 4.5, 
             releaseDate: 'Agosto, 27, 2021'    
           },
           {
             id: 2,
             name: 'Angular: HTTP',
-            imageUrl: '',
+            imageUrl: '/assets/images/http.png',
             price: 45.99,
             code: 'LKL-1094',
             duration: 80,
-            rating: 4,  
+            rating: 3,  
             releaseDate: 'Agosto, 27, 2021'        
           }
             
